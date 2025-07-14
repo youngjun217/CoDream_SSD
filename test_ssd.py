18a2994bd74f80d8a3af5b51744afc44530f1a6c
Kevin108Choi <reason108@naver.com>
2025-07-14 14:16:14 +0900

    [Feature] Add Exeptional cases to test_ssd.py

--- test_ssd.py
+++ test_ssd.py
@@ -1,2 +1,50 @@
+import pytest
+
+from ssd import SSD
+
+
 def test_ssd():
     assert False
+
+def test_ssd_read_error_minus_index():
+    ssd = SSD()
+    with pytest.raises(ValueError, match = "ERROR"):
+        ssd.read(-1)
+    with pytest.raises(ValueError, match = "ERROR"):
+        ssd.read(-10)
+
+def test_ssd_read_error_index_above_99():
+    ssd = SSD()
+    with pytest.raises(ValueError, match = "ERROR"):
+        ssd.read(100)
+    with pytest.raises(ValueError, match="ERROR"):
+        ssd.read(1000)
+    with pytest.raises(ValueError, match = "ERROR"):
+        ssd.read(10000)
+
+def test_ssd_read_error_not_digit():
+    ssd = SSD()
+    with pytest.raises(ValueError, match = "ERROR"):
+        ssd.read("abc")
+
+
+def test_ssd_write_error_minus_index():
+    ssd = SSD()
+    with pytest.raises(ValueError, match = "ERROR"):
+        ssd.write(-1)
+    with pytest.raises(ValueError, match = "ERROR"):
+        ssd.write(-10)
+
+def test_ssd_write_error_index_above_99():
+    ssd = SSD()
+    with pytest.raises(ValueError, match = "ERROR"):
+        ssd.write(100)
+    with pytest.raises(ValueError, match = "ERROR"):
+        ssd.write(1000)
+    with pytest.raises(ValueError, match = "ERROR"):
+        ssd.write(10000)
+
+def test_ssd_write_error_not_digit():
+    ssd = SSD()
+    with pytest.raises("ValueError", match = "ERROR"):
+        ssd.write("abc")

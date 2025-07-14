193fd760fff435ba4be7f33a820a535bcb0ce29a
jihun75.kim <gnswlrla0218@naver.com>
2025-07-14 15:52:44 +0900

    [feature:test_write] test_write()함수 구현. ssd.write()는 mocker patching을 이용하여 대체하고, pass되는 경우와 fail되는 경우 모두 테스트 진행.

--- test_shell.py
+++ test_shell.py
@@ -21,3 +21,17 @@ def test_read_fail(mocker):
         shell.read(100)
     with pytest.raises(ValueError, match='ERROR'):
         shell.read(10.1)
+
+def test_write( mocker):
+    shell = shell_ftn()
+    mk = mocker.patch('ssd.write')
+    shell.write(3, '0x00000000')
+    shell.write(0, '0x00000000')
+    shell.write(3, '0x03300000')
+    with pytest.raises(AssertionError):
+        shell.write(-1, '0x00000000')
+        shell.write(100, '0x00000000')
+        shell.write('3', '0x00000000')
+        shell.write(3, '0x0000000011')
+    mk.call_count == 7
+    pass

ab0e33495388b4f1915ec0eaf406f5f243bb5cb6
h-jaewon <hbom20@naver.com>
2025-07-16 09:23:22 +0900

    [fix] typo

--- test_ssd.py
+++ test_ssd.py
@@ -162,7 +162,7 @@ def test_ssd_write_output(ssd, mocker: MockerFixture):
     ssd._output_txt.write.assert_called_with("")
 
 @pytest.mark.parametrize("output", [["ERROR"], ["10 0x00000000"], ["20 0x00000000"]])
-def test_singletone_ssd_nand(output):
+def test_singleton_ssd_nand(output):
     ssd_output = SSDNand()
     ssd_output.write(output)
     ssd_new_output = SSDNand()

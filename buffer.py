8961ec0de278282d0305e35ff431e3a401a8150c
h-jaewon <hbom20@naver.com>
2025-07-17 16:11:42 +0900

    [feat] buffer flash feature

--- buffer.py
+++ buffer.py
@@ -21,42 +21,8 @@ class Buffer:
         if not os.path.exists(self.folder_path):
             os.makedirs(self.folder_path)
         for i in range(1, 6):
-            file_name = f'{i}_empty'
             file_path = os.path.join(self.folder_path, f'{i}_empty')
             open(file_path, 'a').close()
-            self.buf_lst.append(file_name)
-
-    def write(self, command, lba, value=None):
-        empty_idx = -1
-        for idx, file_name in enumerate(self.buf_lst):
-            splited_file_name = file_name.split("_")
-            if splited_file_name[1] == "empty":
-                empty_idx = idx + 1
-                break
-
-        if empty_idx == -1:
-            self.flush()
-            empty_idx=1
-
-        old_name = f"{self.folder_path}/{empty_idx}_empty"
-        new_name = f"{self.folder_path}/{empty_idx}_{command}_{lba}_{value}"
-        if empty_idx != -1:
-            os.rename(old_name, new_name)
-            self.buf_lst[empty_idx - 1] = f"{empty_idx}_{command}_{lba}_{value}"
-
-    def execute(self):
-        for file_name in self.buf_lst:
-            _, command, lba, value = file_name.split("_")
-            self.ssd.run([None, command, lba, value])
-
-    def flush(self):
-        self.execute()
-        for filename in os.listdir(self.folder_path):
-            file_path = os.path.join(self.folder_path, filename)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-        self.buf_lst.clear()
-        self.create()
 
     def run(self, sys_argv):
         self.set_buffer(sys_argv)
@@ -75,6 +41,7 @@ class Buffer:
         if cmd == "W":
             self.command_memory[lba] = WRITE
             self.value_memory[lba] = value
+            self.ssd._output_txt.write("")
 
         if cmd == "E":
             for erase_lba in range(lba, lba+value):
@@ -110,23 +77,39 @@ class Buffer:
                         self.buf_lst.append(f"{len(self.buf_lst)+1}_E_{start_lba}_{erase_size}")
                         prev_command = EMPTY
 
+        flashed = False
         while len(self.buf_lst) > 5:
-            #flush
-            for filename in os.listdir(self.folder_path):
-                file_path = os.path.join(self.folder_path, filename)
-                if os.path.isfile(file_path):
-                    os.remove(file_path)
-            self.create()
-            contains_W = [elem for elem in self.buf_lst[:5] if "W" in elem]
-            if contains_W:
-                self.ssd._output_txt.write("")
+            #flash
+            flashed = True
+            for i in range(5):
+                _, flushed_cmd, flushed_lba, flushed_value = self.buf_lst[i].split("_")
+                flushed_lba = int(flushed_lba)
+                if flushed_cmd == "W":
+                    self.command_memory[int(flushed_lba)] = EMPTY
+                if flushed_cmd == "E":
+                    flushed_value = int(flushed_value)
+                    for flush_erase_idx in range(flushed_lba, flushed_lba+flushed_value):
+                        self.command_memory[int(flush_erase_idx)] = EMPTY
+
             self.buf_lst = self.buf_lst[5:]
+
+        for buf_idx, buf in enumerate(self.buf_lst):
+            self.buf_lst[buf_idx] = f"{buf_idx+1}_{buf[2:]}"
 
         while len(self.buf_lst) < 5:
             self.buf_lst.append(f"{len(self.buf_lst)+1}_empty")
 
-        for idx, filename in enumerate(os.listdir(self.folder_path)):
-            if self.buf_lst[idx] != filename:
-                os.rename(filename, self.buf_lst[idx])
+
+        if flashed:
+            for filename in os.listdir(self.folder_path):
+                file_path = os.path.join(self.folder_path, filename)
+                os.remove(file_path)
+            for buf in self.buf_lst:
+                file_path = os.path.join(self.folder_path, buf)
+                open(file_path, 'a').close()
 
 
+            for idx, filename in enumerate(os.listdir(self.folder_path)):
+                if self.buf_lst[idx] != filename:
+                    file_path = os.path.join(self.folder_path, filename)
+                    os.rename(file_path, f"{self.folder_path}/{self.buf_lst[idx]}")
